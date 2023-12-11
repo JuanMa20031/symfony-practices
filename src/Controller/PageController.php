f075c365332878d4465a338e4440458d4225f5cd
Juan Manuel <juanma200310@gmail.com>
2023-12-11 15:57:43 -0500

    se crean formularios de lado del servidor

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,15 +15,26 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/')]
-    public function home(EntityManagerInterface $entityManager): Response
+    #[Route('/', name: 'home')]
+    public function home(EntityManagerInterface $entityManager, Request $request): Response
     {
+        $form = $this->createForm(CommentType::class);
+        $form->handleRequest($request);
+    
+        if( $form->isSubmitted() ) {
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+        
+            return $this->redirectToRoute('home');
+        }
+    
         $comments = $entityManager->getRepository(Comment::class)->findBy([],[
             'id' => 'DESC'
         ]);
 
         return $this->render('home.html.twig', [
-            'comments' => $comments
+            'comments' => $comments,
+            'form' => $form->createView()
         ]);
     }
 }
